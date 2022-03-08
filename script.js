3ed079e10e9a8ac69eff360e1d88aa5c14e821c0
Jima Victor <72739268+jimavictor@users.noreply.github.com>
2022-03-08 21:32:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ arr.forEach((element, index) => {
     element.style.opacity = "1";
     if (index == 0) {
       document.getElementsByTagName("body")[0].style.backgroundColor =
-        "rgb(92, 204, 125)";
+        "white";
     } else if (index == 1) {
       document.getElementsByTagName("body")[0].style.backgroundColor = "white";
     } else {
       document.getElementsByTagName("body")[0].style.backgroundColor =
-        "rgb(105, 214, 198)";
+        "rgb(92, 204, 125)";
     }
     arr
       .filter(function (item) {
